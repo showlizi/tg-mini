a46d73237d791eb00a099ca4f8c59d7138161e48
showlizi <103407187+showlizi@users.noreply.github.com>
2023-12-28 03:16:55 +0800

    Update config.ts

--- app/src/config.ts
+++ app/src/config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Welcome to Telegram Onboarding Kit',
+          title: 'Lizi test',
           description:
             "Create stunning onboarding and paywall for your Telegram Bot using the full power of Mini Apps<br><br>It's <b>simple</b>, <b>fast</b>, highly <b>customizable</b> and <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>open-source</a>!",
           button: 'Next',
